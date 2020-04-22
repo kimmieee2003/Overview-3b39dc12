b48dfae9e6086c1f361f485ff97276efb37b1ef3
Kimberly Hogeveen <2103405@talnet.nl>
2020-04-22 15:16:26 +0200

    Uitwerking van de exercise

--- index.php
+++ index.php
@@ -1,11 +1,67 @@
 <?php
-$dbh = new PDO(
-    'mysql:host=127.0.0.1;port:3306;dbname=netland', 
-    'root', 
-    ''
-);
-echo 'connected to Netland';
-echo $dbh->query('select version()')->fetchColumn();
+$host = '127.0.0.1:3306';
+$db   = 'netland';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+    $dbh = new PDO($dsn, $user, $pass, $options);
+    echo("Connected to: " . $db . " on " . $host . " version: " . phpversion());
+    echo("<br>");
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welkom op het netland beheerspaneel</h1>
+    <h2>Series</h2>
+    <table style="width:50%">
+    <tr>
+    <td>Titel</td>
+    <td>Rating</td>
+</tr>
+    <?php 
+    
+    $file ='import.sql';
+    $stmt = $dbh->query('SELECT title, rating FROM series');
+    while ($row = $stmt->fetch())
+    {
+        echo '<tr><td>' . $row['title'] . '</td><td>' . $row['rating'] . '</td></tr>\n';
+        
+    }
+    
+?>
+</table>
+    <h2>Films</h2>
+    <table style="width:50%">
+ <tr>
+  <td>Titel</td>
+  <td>Duur</td>
+
+<?php 
+    $file ='import.sql';
+    $stmt = $dbh->query('SELECT naam, duur_minuten FROM films');
+    while ($row = $stmt->fetch())
+    {
+        echo '<tr><td>' . $row['naam'] . '</td><td>' . $row['duur_minuten'] . '</td></tr>\n';
+    }
+?>
+</table>
+</body>
+</html>
